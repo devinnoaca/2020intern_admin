3efa53f2653ab02dad42215a1f115434eaac3359
이정현 <leejh@ijeonghyeon-ui-MacBookPro.local>
2020-07-28 10:31:12 +0900

    apply bootstrap templates & split by component

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import router from './app';
 import * as express from 'express';
 import db from './db';
+import * as path from 'path';
 
 const app = express();
 app.use('/', router);
 
 db.init();
 
+app.set('views', path.join(__dirname, '/Views'))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/Public'));
+
 app.get('/', (request: express.Request, response: express.Response) => { 
-	response.send('Hello World!');
+	// response.send('Hello World!');
+	response.render('dashboard');
 });
 app.listen(3000, () => console.log(`start`));
