8b1912837e812280486209dd241cd5d713b1acb5
이진구 <ijingu@ijinguui-MacBookPro.local>
2020-07-28 12:00:02 +0900

    index.ts 라우팅 메소드 확인으로 인해 원상태로 복구

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import router from './app';
 import * as express from 'express';
-import db from './db';
+//import db from './db';
 import * as path from 'path';
 
 const app = express();
 app.use('/', router);
 
-db.init();
+//db.init();
 
 app.set('views', path.join(__dirname, '/views'))
 app.set("view engine", "ejs");
@@ -15,10 +15,6 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (request: express.Request, response: express.Response) => { 
 	// response.send('Hello World!');
 	response.render('dashboard');
-});
-
-app.get('/user', (request: express.Request, response: express.Response) => { 
-	response.render('usermanage');
 });
 
 app.listen(3000, () => console.log(`start`));
