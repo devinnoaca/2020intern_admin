c3318cefef63b4e997759ac5a60e5b73b0faefba
labiss96 <cxz9080@gmail.com>
2020-07-29 13:16:18 +0900

    modify keyword router and controller

--- src/controllers/Keyword.ts
+++ src/controllers/Keyword.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 
-const createKeyword = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
-  response.status(200).send(
+const createKeyword = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  res.status(200).send(
         {
           'status': 200
         }
@@ -9,8 +9,8 @@ const createKeyword = (request: express.Request, response: express.Response, nex
   console.log('createKeyword');
 };
 
-const deleteKeyword = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
-  response.status(200).send(
+const deleteKeyword = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  res.status(200).send(
         {
           'status': 200
         }
@@ -18,8 +18,35 @@ const deleteKeyword = (request: express.Request, response: express.Response, nex
   console.log('deleteKeyword');
 };
 
+const getKeyword = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  console.log('run getKeyword API');
+  res.status(200).render('keyword', 
+    {
+      'status': 200,
+      'data': [
+        {
+            category: '개발/프로그래밍', 
+            keyword : ['java', 'javascript', 'python']
+        },
+        {
+            category: '디자인', 
+            keyword : ['html', 'CSS', 'SCSS']
+        },
+        {
+            category: '데이터사이언스', 
+            keyword : ['MongoDB', 'MySQL', 'MariaDB', 'SQL', 'RDBMS', 'Oracle', 'R']
+        },
+        {
+            category: '업무스킬', 
+            keyword : ['프로젝트 관리', '데이터 분석', '정보보안', 'VBA']
+        },
+      ],
+    });
+}
+
 const router = express.Router();
 
+router.get('/', getKeyword);
 router.post('/', createKeyword);
 router.delete('/:id', deleteKeyword);
 
