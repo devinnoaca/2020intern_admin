b14951937708f244e414cf6a0c8f13778d3f2cc9
labiss96 <cxz9080@gmail.com>
2020-07-29 14:30:47 +0900

    modify JSON response structure in keyword controller. implement keyword delete using Ajax

--- src/controllers/Keyword.ts
+++ src/controllers/Keyword.ts
@@ -11,35 +11,38 @@ const createKeyword = (req: express.Request, res: express.Response, next: expres
 };
 
 const deleteKeyword = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  console.log('run deleteKeyword API');
+  console.log(`'${req.params.name}' 키워드 삭제 요청`);
   res.status(200).send(
         {
-          'status': 200
+          'status': 200,
+          'result': "삭제 성공"
         }
       );
-  console.log('deleteKeyword');
+  
 };
 
-const getKeyword = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
-  console.log('run getKeyword API');
+const getKeywords = (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  console.log('run getKeywords API');
   res.status(200).render('keyword',
     {
       'status': 200,
       'data': [
         {
             category: '개발/프로그래밍', 
-            keyword : ['java', 'javascript', 'python']
+            keyword : ['java_개발', 'javascript_개발', 'python_개발']
         },
         {
             category: '디자인', 
-            keyword : ['html', 'CSS', 'SCSS']
+            keyword : ['html_디자인', 'CSS_디자인', 'SCSS_디자인']
         },
         {
             category: '데이터사이언스', 
-            keyword : ['MongoDB', 'MySQL', 'MariaDB', 'SQL', 'RDBMS', 'Oracle', 'R']
+            keyword : ['MongoDB_데이터사이언스', 'MySQL_데이터사이언스', 'MariaDB_데이터사이언스', 'SQL_데이터사이언스', 'RDBMS_데이터사이언스', 'Oracle_데이터사이언스', 'R_데이터사이언스']
         },
         {
             category: '업무스킬', 
-            keyword : ['프로젝트 관리', '데이터 분석', '정보보안', 'VBA']
+            keyword : ['프로젝트관리_업무스킬', '데이터분석_업무스킬', '정보보안_업무스킬', 'VBA_업무스킬']
         },
       ],
     });
@@ -60,9 +63,9 @@ const searchKeyword = (req: express.Request, res: express.Response, next: expres
 
 const router = express.Router();
 
-router.get('/', getKeyword);
+router.get('/', getKeywords);
 router.post('/', createKeyword);
-router.delete('/:id', deleteKeyword);
+router.delete('/:name', deleteKeyword);
 router.post('/search', searchKeyword);
 
 export = router;
