27b8b0d0a4ba538ca5e5cf7559cad6fb8c43ad50
labiss96 <cxz9080@gmail.com>
2020-07-31 10:08:10 +0900

    set responseType JSON in Ajax

--- src/public/js/common/ajax.js
+++ src/public/js/common/ajax.js
@@ -3,6 +3,7 @@ const sendAjax = (method, url, data, callback) => {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
     xhr.setRequestHeader('Content-type', "application/json");
+    xhr.responseType = 'json';
     xhr.send(data);
 
     // 데이터 수신이 완료되면 표시
