0fa340101df074130883da27b650a55309d06ba4
labiss96 <cxz9080@gmail.com>
2020-07-31 10:04:25 +0900

    modularize javascript, modal in keyword.ejs. set js path in index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ app.use('/', router);
 app.set('views', path.join(__dirname, '/views'))
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/public/js'));
 
 app.get('/', (req: express.Request, res: express.Response) => { 
 	res.render('dashboard');
---
+++ src/public/js/common/ajax.js
@@ -0,0 +1,12 @@
+const sendAjax = (method, url, data, callback) => {
+    // content-type을 설정하고 데이터 송신
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader('Content-type', "application/json");
+    xhr.send(data);
+
+    // 데이터 수신이 완료되면 표시
+    xhr.addEventListener('load', () => {
+        callback(xhr);
+    });
+}
