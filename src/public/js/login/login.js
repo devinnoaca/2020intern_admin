771e2ed6cc135a57925e9236f765fbadfce7df13
labiss96 <cxz9080@gmail.com>
2020-08-12 15:07:00 +0900

    로그인 요청 ajax통신 및 callback 구현

---
+++ src/public/js/login/login.js
@@ -0,0 +1,18 @@
+const loginCallback = (xhr) => {
+    if(xhr.status == 200) {
+      alert(`환영합니다!`);
+      window.location.href = '/matching';
+    } else {
+      alert(`로그인 실패 [${xhr.status}]`);
+    }
+  }
+
+const onLogin = () => {
+    const formData = document.loginForm;
+    const jsonData = {
+        id : formData.id.value,
+        password: formData.password.value
+    };
+
+    sendAjax('POST', `/login`, JSON.stringify(jsonData), loginCallback);
+}
