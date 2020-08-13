811a639c18584dedaad26806a2ed64a96508a57f
LJG070 <dlwlsrn94@naver.com>
2020-08-13 11:20:22 +0900

    career-event.js 필요한 모듈들 임포트

--- src/public/js/career/career-create.js
+++ src/public/js/career/career-create.js
@@ -1,3 +1,6 @@
+import {careerClickEvent} from './career-event.js';
+import {modifyCareerCallback} from './career-modify.js';
+
 //커리어 추가시 콜백 함수
 const addCareerCallback = function(xhr) {
   const status = xhr.status;
