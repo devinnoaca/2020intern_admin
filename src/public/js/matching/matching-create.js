201e7f1e2f2dfc8686daecf9141cccc668c38b72
labiss96 <cxz9080@gmail.com>
2020-08-13 10:35:57 +0900

    modify matching callback

--- src/public/js/matching/matching-create.js
+++ src/public/js/matching/matching-create.js
@@ -17,13 +17,19 @@ for (var i = 0; i < stateRadio.length; ++i) {
 
 
 createMatchingCallback = (xhr) => {
-  if (xhr.status == 200) {
-    console.log(xhr.response.message);
-    alert(`매칭정보가 정상적으로 생성되었습니다.`);
-    window.location.href = '/matching';
-  } else {
-    alert(`매칭정보 생성 실패 [${xhr.status}]`);
-    console.log(xhr.response.message);
+  const status = xhr.status;
+  const message = xhr.response.message;
+
+  switch (status) {
+    case 200:
+      alert(`매칭정보가 정상적으로 생성되었습니다`);
+      console.log(message);
+      window.location.href = '/matching';
+      break;
+
+    case 400:
+      alert(`생성 실패 : ${message}`);
+      break;
   }
 }
 
@@ -33,21 +39,21 @@ const onCreate = () => {
   let isChecked = true;
 
   //매칭요청이 대기 상태로 생성 될 시, 읽지 않음으로 생성
-  if(formData.state.value == 0) {
+  if (formData.state.value == 0) {
     isChecked = false;
   }
 
   const jsonData = {
     state: formData.state.value,
     mentee_ID: formData.menteeId.value,
-    mentor_ID: formData.mentorId.value, 
+    mentor_ID: formData.mentorId.value,
     request_time: new Date(),
     is_checked: isChecked,
     request_message: formData.requestMessage.value,
     response_message: formData.responseMessage.value
   }
 
-  if(matchingDataValidation(jsonData)) {
+  if (matchingDataValidation(jsonData)) {
     sendAjax('POST', `/matching`, JSON.stringify(jsonData), createMatchingCallback);
   }
 
