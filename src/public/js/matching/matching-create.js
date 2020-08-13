967f3dcf083a71f7268a6e12fa468734798d0a58
labiss96 <cxz9080@gmail.com>
2020-08-13 10:28:34 +0900

    fix convention name, id attribute in matchingCreateModal

--- src/public/js/matching/matching-create.js
+++ src/public/js/matching/matching-create.js
@@ -1,16 +1,16 @@
 // 매칭정보의 상태가 거절일 경우, 거절사유 입력폼을 보여준다.
 
 const stateRadio = document.createForm.state;
-const rejectInput = document.querySelector('#reject-input-form');
+const responseInput = document.querySelector('#responseInputLi');
 
 for (var i = 0; i < stateRadio.length; ++i) {
   stateRadio[i].onclick = function () {
     switch (this.value) {
       case '2':
-        rejectInput.style.display = "block";
+        responseInput.style.display = "block";
         break;
       default:
-        rejectInput.style.display = "none";
+        responseInput.style.display = "none";
     }
   }
 }
