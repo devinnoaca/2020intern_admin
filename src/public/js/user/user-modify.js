18bad65320817af9894e73621614874f626374d5
LJG070 <dlwlsrn94@naver.com>
2020-08-20 16:32:29 +0900

    수정

--- src/public/js/user/user-modify.js
+++ src/public/js/user/user-modify.js
@@ -4,10 +4,43 @@ import {addCareerCallback} from "../career/career-create.js";
 import {modifyCareerCallback} from "../career/career-modify.js";
 import {deleteCareerCallback} from "../career/career-delete.js";
 
+Date.prototype.format = function (f) {
+
+  if (!this.valueOf()) return " ";
+  var weekKorName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+  var weekKorShortName = ["일", "월", "화", "수", "목", "금", "토"];
+  var weekEngName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var weekEngShortName = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var d = this;
+  return f.replace(/(yyyy|yy|MM|dd|KS|KL|ES|EL|HH|hh|mm|ss|a\/p)/gi, function ($1) {
+      switch ($1) {
+          case "yyyy": return d.getFullYear(); // 년 (4자리)
+          case "yy": return (d.getFullYear() % 1000).zf(2); // 년 (2자리)
+          case "MM": return (d.getMonth() + 1).zf(2); // 월 (2자리)
+          case "dd": return d.getDate().zf(2); // 일 (2자리)
+          case "KS": return weekKorShortName[d.getDay()]; // 요일 (짧은 한글)
+          case "KL": return weekKorName[d.getDay()]; // 요일 (긴 한글)
+          case "ES": return weekEngShortName[d.getDay()]; // 요일 (짧은 영어)
+          case "EL": return weekEngName[d.getDay()]; // 요일 (긴 영어)
+          case "HH": return d.getHours().zf(2); // 시간 (24시간 기준, 2자리)
+          case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2); // 시간 (12시간 기준, 2자리)
+          case "mm": return d.getMinutes().zf(2); // 분 (2자리)
+          case "ss": return d.getSeconds().zf(2); // 초 (2자리)
+          case "a/p": return d.getHours() < 12 ? "오전" : "오후"; // 오전/오후 구분
+          default: return $1;
+      }
+  });
+};
+
+String.prototype.string = function (len) { var s = '', i = 0; while (i++ < len) { s += this; } return s; };
+String.prototype.zf = function (len) { return "0".string(len - this.length) + this; };
+Number.prototype.zf = function (len) { return this.toString().zf(len); };
+
 const url = window.location.href;
 const urlSplit = url.split('/');
 const usn = urlSplit[urlSplit.length - 1];
-
+let date = new Date();
+date = date.format('yyyy-MM-dd HH:mm:ss');
 //매칭 열람하기 링크 초기화
 (function(){
   const id = $('[name="id"]').val();
@@ -15,12 +48,10 @@ const usn = urlSplit[urlSplit.length - 1];
   let queryString = '';
   switch (type) {
     case '0':
-      queryString += `queryType=SEARCH&mentorID=&menteeID=${id}&state=-1
-                      &isTotal=on&startDateSubmit=1970-01-01+00%3A00%3A00&endDateSubmit=2020-08-14+06%3A59%3A45`;
+      queryString += `queryType=SEARCH&page=1&range=1&mentorID=&menteeID=${id}&state=-1&isTotal=on&startDateSubmit=1970-01-01&endDateSubmit=${date}`;
       break;
     case '1':
-      queryString += `queryType=SEARCH&mentorID=${id}&menteeID=&state=-1
-                      &isTotal=on&startDateSubmit=1970-01-01+00%3A00%3A00&endDateSubmit=2020-08-14+06%3A59%3A45`;
+      queryString += `queryType=SEARCH&page=1&range=1&mentorID=${id}&menteeID=&state=-1&isTotal=on&startDateSubmit=1970-01-01&endDateSubmit=${date}`;
       break;
     default:
       break;
